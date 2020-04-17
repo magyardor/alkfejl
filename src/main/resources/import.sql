5d6b451ead10f9f5fdd070ccdd49d3b182e2d7d0
magyardor <61864184+magyardor@users.noreply.github.com>
2020-04-17 23:06:28 +0200

    v

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into user (username, password, enabled, role) values ('user1', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_ADMIN');
+insert into user (username, password, enabled, role) values ('user2', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', true, 'ROLE_USER'); 
